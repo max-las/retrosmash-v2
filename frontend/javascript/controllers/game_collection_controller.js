156a0ea702405f34ec75d1f8a4d367e31dc8fcc7
Maxime <lasserre.maxime@yahoo.fr>
2025-03-26 23:52:26 +0100

    store games

--- frontend/javascript/controllers/game_collection_controller.js
+++ frontend/javascript/controllers/game_collection_controller.js
@@ -4,16 +4,48 @@ import { db } from "../db";
 export default class extends Controller {
   static values = {
     collectionUrl: String,
-    versionUrl: String
+    versionUrl: String,
+    consoleSlug: String
   }
 
   connect() {
-    this.fetchVersion();
+    this.syncCollection();
+  }
+
+  async syncCollection() {
+    const version = await this.fetchVersion();
+    const gameCollection = await db.gameCollections.get(this.consoleSlugValue);
+    if (gameCollection && gameCollection.version === version) { return; }
+
+    return await Promise.all([this.updateGameCollection(version), this.updateGames()]);
+  }
+
+  async updateGameCollection(version) {
+    const newGameCollection = { console_slug: this.consoleSlugValue, version };
+    return await db.gameCollections.put(newGameCollection);
+  }
+
+  async updateGames() {
+    const [games] = await Promise.all([this.fetchGames(), this.clearGames()]);
+    return await Promise.all(games.map((game) => this.addGame(game)));
   }
 
   async fetchVersion() {
     const response = await fetch(this.versionUrlValue, { method: "GET" });
-    const version = await response.text();
-    console.log(version);
+    return await response.text();;
+  }
+
+  async fetchGames() {
+    const response = await fetch(this.collectionUrlValue, { method: "GET" });
+    return await response.json();
+  }
+
+  async clearGames() {
+    return await db.games.where('console_slug').equals(this.consoleSlugValue).delete();
+  }
+
+  async addGame(game) {
+    game.console_slug = this.consoleSlugValue;
+    return await db.games.add(game);
   }
 }
--- frontend/javascript/db.js
+++ frontend/javascript/db.js
@@ -3,6 +3,6 @@ import Dexie from 'dexie';
 export const db = new Dexie('retrosmash');
 
 db.version(1).stores({
-  game_collections: 'console_slug',
+  gameCollections: 'console_slug',
   games: '[console_slug+slug], console_slug, players'
 });
