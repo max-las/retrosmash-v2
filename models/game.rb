e669e1d7251df62a145c0054a07b10c0c7676065
Maxime <lasserre.maxime@yahoo.fr>
2025-04-23 19:28:24 +0200

    remove useless method

--- models/game.rb
+++ models/game.rb
@@ -1,8 +1,4 @@
 class Game < Bridgetown::Model::Base
-  def self.from_data(data:, console:)
-    new(**data, data:, console:)
-  end
-
   def to_json(*_args)
     data.merge(console_slug: console.slug).to_json
   end
